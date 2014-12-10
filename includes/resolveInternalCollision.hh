5867bafd0a9a19f393dcefc8f76cb78457016463
Luis Sanabria-Russo <luis.sanabria@upf.edu>
2014-12-10 19:10:19 +0100

    Fixed and reduced its size

--- includes/resolveInternalCollision.hh
+++ includes/resolveInternalCollision.hh
@@ -1,59 +1,52 @@
-#include <math.h>
-#include <algorithm>
-
 #define AC 4
 #define MAXSTAGE 5
 
-int resolveInternalCollision(std::array<int,AC> &backlogg, std::array<FIFO <Packet>, AC> &Queues, 
-	std::array<int,AC> &stickiness, std::array<int,AC> &stages, std::array<double,AC> &counters, 
-	int system_stickiness, int id, std::array<double,AC> &totalInternalACCol, std::array<int,AC> &retAttemptAC,
-	double simTime, int ECA,std::array<int,AC> &recomputeBackoff){
+int resolveInternalCollision(std::array<double,AC> &counters, std::array<int,AC> &backlogg, std::array<int,AC> &stickiness, 
+	std::array<int,AC> &stages, std::array<int,AC> &recomputeBackoff, std::array<double,AC> &totalInternalACCol,
+	std::array<int,AC> &retAttemptAC){
 
 	int iterator = counters.size() - 1;
-	int acToTx = -1;
+	int acToTx;
+	int winner = -1;
 
-	for (auto rIterator = counters.rbegin(); rIterator < counters.rend(); rIterator++) //a reverse iteration over the arrays
+	for(int i = iterator; i >= 0; i--)
 	{
-		//Checks if an AC is backlogged AND its backoff counter expired
-		//Because it's going backwards, it just need to find the firts (highes priority) one.
-		if((backlogg.at(iterator) == 1) && (*rIterator == 0))
-		{
-			acToTx = iterator;
-			//cout << "(" << simTime << ") STA-" << id << ": AC: " << iterator << " backlogg: " << backlogg.at(iterator) << endl;
-			break;
-		}
-		iterator--;
-	}
-
-
-	//Checks if there are any other ACs with an expired backoff. If so, they are treated as internal collisions
-	if(iterator >= 0)
-	{
-		for(int i = 0; i < iterator; i++)
-		{
-			if((backlogg.at(i) == 1) && (counters.at(i) == 0))
+		if(backlogg.at(i) == 1)
+		{ 
+			if(counters.at(i) == 0) 
 			{
-				//cout << "(" << simTime << ") STA-" << id << ": Colliding internally with " << acToTx << ": " << i 
-				//	<< " with counter: " << counters.at(i) << " and stage: " << stages.at(i) << endl;
-
-				stickiness.at(i) = std::max((int) stickiness.at(i) - 1, 0);
-	            stages.at(i) = std::min((int)stages.at(i) + 1, MAXSTAGE);
-	            //cout << "STA-" << id << ": internalCollision: " << acToTx << " and " << i << endl;
-				
-				recomputeBackoff.at(i) = 1;
-
-				// computeBackoff(backlogg.at(i), Queues.at(i), i, stickiness.at(i), stages.at(i), 
-					// counters.at(i), system_stickiness, id, sx, ECA);
-				//cout << ". New counter for AC " << i << ": " << counters.at(i) << ", new stage: " << stages.at(i) << endl;
-				totalInternalACCol.at(i)++;
-
-				retAttemptAC.at(i)++;
-			}else
-			{
-				recomputeBackoff.at(i) = 0;
+				winner = i; 
+				break;
 			}
 		}
 	}
+
+	acToTx = winner;
+
+	if(acToTx > 0)
+	{
+		for(int i = 0; i < acToTx; i++)
+		{
+			if(backlogg.at(i) == 1)
+			{
+				if(counters.at(i) == 0)
+				{
+					stickiness.at(i) = std::max((int) stickiness.at(i) - 1, 0);
+					stages.at(i) = std::min((int)stages.at(i) + 1, MAXSTAGE);
+				
+					recomputeBackoff.at(i) = 1;
+					totalInternalACCol.at(i)++;
+
+					retAttemptAC.at(i)++;
+				}else
+				{
+					recomputeBackoff.at(i) = 0;
+				}
+			}
+		}
+	}
+
+
 
 	return(acToTx);
 	
